df05802ff590ce9b64be88af4210bc918aff435a
smitgor <smitgor120@gmail.com>
2021-10-14 01:02:08 +0530

    server configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.get('/api',function(req,res){
     res.json({message: "it's wokring"});
